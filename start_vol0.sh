d3e4f5d49cad904548ffa41a0dfd96005ea16160
Savio Monteiro <savimonty@gmail.com>
2021-10-25 03:41:15 -0700

    commit

--- start_vol0.sh
+++ start_vol0.sh
@@ -5,5 +5,5 @@ setfattr -x trusted.gfid /data/gv0
 
 rm -rf /data/gv0/.glusterfs
 
-gluster volume create gv0 ${SERVER_HOSTNAME}:/data/gv0
+gluster volume create gv0 "${SERVER_HOSTNAME}:/data/gv0"
 gluster volume start gv0
